6f4c59fe88218e9fa63d362443269f789f58c07d
Anna <bonaquista@lsc.gov>
2021-04-26 16:01:30 -0400

    button/icon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn dark depressed color="pink">Click me</v-btn>
+    <v-btn flat color="pink">Click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon>email</v-icon>
+    </v-btn>
   </div>
 </template>
 
