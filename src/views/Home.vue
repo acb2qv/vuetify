c00f02a11ad3c29e80429c1a567bd10f2b003287
Anna <bonaquista@lsc.gov>
2021-04-28 10:35:56 -0400

    add toolbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn dark depressed color="pink">Click me</v-btn>
-    <v-btn flat color="pink">Click me</v-btn>
 
-    <v-btn depressed class="pink white--text">
-      <v-icon>email</v-icon>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
+    <v-btn class="hidden-sm-only">click me</v-btn>
+
   </div>
 </template>
 
