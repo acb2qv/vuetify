48ef5833f4d22cb51a007033b6b9893c2cb50cc5
Anna <bonaquista@lsc.gov>
2021-04-28 12:04:42 -0400

    list of pages in nav drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,6 @@
 <template>
     <nav>
         <v-app-bar app>
-        <!-- <v-toolbar text app> -->
 
             <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer">
             </v-app-bar-nav-icon>
@@ -15,11 +14,19 @@
                 <span>Sign Out</span>
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
-        <!-- </v-toolbar> -->
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
-            <p>test</p>
+            <v-list>
+                <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-tile-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
         </v-navigation-drawer>
 
     </nav>
@@ -30,7 +37,12 @@
 export default {
     data(){
         return{
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'dashboard', text: 'Dashboard', route: '/'},
+                { icon: 'folder', text: 'My Projects', route: '/projects'},
+                { icon: 'person', text: 'Team', route: '/team'},
+            ]
         }
     }
 }
